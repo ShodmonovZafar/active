7c16a0f8cfdc2bf30d296e49aff9449d22047cff
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2022-12-09 20:16:27 +0500

    Yangilandi.

---
+++ main.py
@@ -0,0 +1,63 @@
+
+class Number:
+    def __init__(self, number: int) -> None:
+        self.number = number
+    
+    def get_number(self):
+        return self.number
+    
+    def is_even(self):
+        if self.number % 2 == 0:
+            return True
+        else:
+            return False
+    
+    def is_odd(self):
+        if self.number % 2 == 0:
+            return False
+        else:
+            return True
+    
+    def is_prime(self):
+        pass
+    
+    def get_divisors(self):
+        pass
+    
+    def get_length(self):
+        pass
+    
+    def get_sum(self):
+        pass
+    
+    def get_product(self):
+        pass
+    
+    def get_reverse(self):
+        pass
+    
+    def get_digits(self):
+        pass
+    
+    def get_max(self):
+        pass
+    
+    def get_min(self):
+        pass
+    
+    def get_average(self):
+        pass
+    
+    def get_median(self):
+        pass
+    
+    def get_mode(self):
+        pass
+    
+    def get_range(self):
+        pass
+    
+    def get_frequency(self):
+        pass
+
+
